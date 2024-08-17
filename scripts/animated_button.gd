141e70a4d5b6a8d0dd37df094d8609edc9a5ab24
= <vegotenksGogeto@gmx.de>
2024-08-17 15:44:17 +0200

    Animated hover buttons, main menu

---
+++ scripts/animated_button.gd
@@ -0,0 +1,29 @@
+extends Button
+class_name AnimatedButton
+
+var growTween: Tween
+var shrinkTween: Tween
+
+func _ready() -> void:
+	focus_entered.connect(_on_focus_entered)
+	focus_exited.connect(_on_focus_exited)
+
+func _process(delta: float) -> void:
+	pivot_offset = size / 2
+
+func _on_focus_entered() -> void:
+	if shrinkTween != null and shrinkTween.is_running():
+		shrinkTween.stop()
+
+	growTween = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_BOUNCE)
+	growTween.tween_property(self, "scale", Vector2(1.02, 1.02), 0.5)
+	pass
+
+
+func _on_focus_exited() -> void:
+	if growTween != null and growTween.is_running():
+		growTween.stop()
+
+	shrinkTween = create_tween().bind_node(self).set_trans(Tween.TRANS_BOUNCE)
+	shrinkTween.tween_property(self, "scale", Vector2(1., 1.), 0.15)
+	pass
