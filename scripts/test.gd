a102c4b64b522511fb75b7be568fcbc99f69b1ce
= <vegotenksGogeto@gmx.de>
2024-08-17 14:30:24 +0200

    Unused parameter

--- scripts/test.gd
+++ scripts/test.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if $Player.is_dash_active():
 		$DashBar.value = abs(1 - ($Player.DASH_DURATION_MILLIS - $Player.dash_active_time_remaining()) / float($Player.DASH_DURATION_MILLIS))
 	elif $Player.dash_timeout_remaining() == 0:
