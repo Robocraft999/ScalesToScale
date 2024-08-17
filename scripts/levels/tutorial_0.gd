64093873aeae0d489073d505690c932de8672bab
VegOwOtenks <vegotenksGogeto@gmx.de>
2024-08-18 00:14:17 +0200

    Almost finishable tutorial

--- scripts/levels/tutorial_0.gd
+++ scripts/levels/tutorial_0.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+@export var finish_enabled := false
+
+func _ready() -> void:
+	pass
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$UpKeyCap.visible = ProgressStore.jump_enabled
