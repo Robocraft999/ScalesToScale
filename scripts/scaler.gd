7ddc5f3561e2352557151a84e62fba93c703ce7b
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2024-08-17 20:07:34 +0200

    added tileable scaling

--- scripts/scaler.gd
+++ scripts/scaler.gd
@@ -6,10 +6,14 @@ const STEP = 0.25
 const MAX_SCALE = 5
 
 var offset := Vector2.ZERO
+var offset_offset = offset
 var new_scale := Vector2.ONE
 
 @onready var parent: CollisionObject2D = $".."
 @onready var line: Line2D = $Line2D
+@onready var sprite: Sprite2D = $"../Sprite2D"
+@onready var sprite_region_size: Vector2 = sprite.region_rect.size
+@onready var collider_size: Vector2 = parent.get_child(0).shape.get_rect().size
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,13 +27,45 @@ func _ready() -> void:
 	#parent.input_event.connect(func(viewport, event, idx): print(event))
 
 func _physics_process(delta: float) -> void:
+	if editing:
+		var toggled = Input.is_action_pressed("scale_toggle")
+		if toggled:
+			$yArrow.visible = true
+			$xArrow.visible = false
+		else:
+			$yArrow.visible = false
+			$xArrow.visible = true
+		
+		offset_offset = offset
+		if Input.is_action_just_released("mouse_wheel_up"):
+			if toggled:
+				offset.y += STEP
+			else:
+				offset.x += STEP
+		elif Input.is_action_just_released("mouse_wheel_down"):
+			if toggled:
+				offset.y -= STEP
+			else:
+				offset.x -= STEP
+		
+		offset.x = clamp(offset.x, 0, 1)
+		offset.y = clamp(offset.y, 0, 1)
+		offset_offset -= offset
+	else:
+		$yArrow.visible = false
+		$xArrow.visible = false
+		
+	
+	new_scale = Vector2(lerp(1, MAX_SCALE, offset.x), lerp(1, MAX_SCALE, offset.y))
+	
 	if not parent.is_on_floor():
 		parent.velocity += parent.get_gravity() * delta
 	parent.move_and_slide()
 	
-	var collider_size = parent.get_child(0).shape.get_rect().size
-	var motion = try_scale(collider_size)
+	var motion = try_scale()
 	if motion != Vector2.ZERO:
+		if motion.y != 0:
+			print(parent.position, motion, collider_size.y * parent.scale.y/2)
 		parent.move_and_collide(motion)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -45,38 +81,9 @@ func _process(delta: float) -> void:
 			editing = true
 			break
 			
-	#if Input.is_action_just_pressed("dash"):
-		#print(offset)
-	
-	if editing:
-		var toggled = Input.is_action_pressed("scale_toggle")
-		if toggled:
-			$yArrow.visible = true
-			$xArrow.visible = false
-		else:
-			$yArrow.visible = false
-			$xArrow.visible = true
-		
-		if Input.is_action_just_released("mouse_wheel_up"):
-			if toggled:
-				offset.y += STEP
-			else:
-				offset.x += STEP
-		elif Input.is_action_just_released("mouse_wheel_down"):
-			if toggled:
-				offset.y -= STEP
-			else:
-				offset.x -= STEP
-		offset.x = clamp(offset.x, 0, 1)
-		offset.y = clamp(offset.y, 0, 1)
-	else:
-		$yArrow.visible = false
-		$xArrow.visible = false
-		
-	new_scale = Vector2(lerp(1, MAX_SCALE, offset.x), lerp(1, MAX_SCALE, offset.y))
 	global_scale = Vector2.ONE
 	
-func try_scale(collider_size: Vector2) -> Vector2:
+func try_scale() -> Vector2:
 	if new_scale == parent.scale:
 		return Vector2.ZERO
 	
@@ -84,8 +91,11 @@ func try_scale(collider_size: Vector2) -> Vector2:
 	var scale_down = new_scale.x < parent.scale.x or new_scale.y < parent.scale.y
 	
 	if scale_down:
-		parent.scale = new_scale
+		scale_objects()
 		print("downscale")
+		var motion_down = Vector2(0, collider_size.y * new_scale.y/2 - 0.1)
+		if not test_move(motion_down):
+			return motion_down
 		return Vector2.ZERO
 	
 	# get expansion size (half)
@@ -94,25 +104,30 @@ func try_scale(collider_size: Vector2) -> Vector2:
 	line.add_point(position)
 	line.add_point(position + expansion)
 	
-	# check if can expand to both sides
-	if not test_move(expansion * -1) and not test_move(expansion):
-		parent.scale = new_scale
-		print("expanding both sides")
-		return Vector2.ZERO
-	
 	# check sides respectively with doubled expansion
 	if not test_move(expansion * -2):
-		parent.scale = new_scale
+		scale_objects()
 		print("expanding left")
 		return expansion * -1
 	elif not test_move(expansion * 2):
-		parent.scale = new_scale
+		scale_objects()
 		print("expanding right")
 		return expansion
+		
+	# check if can expand to both sides
+	if not test_move(expansion * -1) and not test_move(expansion):
+		scale_objects()
+		print("expanding both sides")
+		return Vector2.ZERO
 	
 	# no expansion possible
-	
+	offset += offset_offset
 	return Vector2.ZERO
 	
 func test_move(motion: Vector2) -> bool:
-	return parent.test_move(parent.transform, motion, null, 0, true)
+	return parent.test_move(parent.transform, motion, null, 0, false)
+	
+func scale_objects():
+	parent.scale = new_scale
+	sprite.global_scale = Vector2.ONE
+	sprite.region_rect.size = sprite_region_size * new_scale
