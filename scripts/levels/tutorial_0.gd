fbac47397ba37d4229fb79702e4b9ea5a0a74085
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2024-08-18 00:43:34 +0200

    added simple ending to tutorial

--- scripts/levels/tutorial_0.gd
+++ scripts/levels/tutorial_0.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-@export var finish_enabled := false
-
 func _ready() -> void:
 	pass
 
@@ -12,3 +10,15 @@ func _process(delta: float) -> void:
 # Collectible pickup
 func handle(player: Player):
 	ProgressStore.jump_enabled = true
+
+
+func _on_dragon_flag_body_entered(body: Node2D) -> void:
+	if body is Player:
+		var cam: PhantomCamera2D = $PhantomCamera2D
+		cam.follow_target = $DragonFlag
+		var tween = create_tween()
+		tween.tween_property(cam, "zoom", Vector2(3,3), 3)
+		tween.chain().tween_property(body, "position", Vector2(body.position.x, 0), 3)
+		tween.finished.connect(func(): OptionsManager.get_scene_manager().load_level_scene_by_index(0))
+		
+		
