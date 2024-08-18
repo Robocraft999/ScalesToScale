6763148395578c0a5ab2be0b1f0b4215769334a8
VegOwOtenks <vegotenksGogeto@gmx.de>
2024-08-19 01:26:19 +0200

    Finishing touch on level 3, intro animation, correct player position

--- scripts/levels/level_3.gd
+++ scripts/levels/level_3.gd
@@ -11,5 +11,11 @@ func _trigger_animation(body: Node2D):
 		$TypewriterAnimationPlayer.play("typewriter_animation")
 		$TypewriterTrigger.body_entered.disconnect(_trigger_animation)
 		await $TypewriterAnimationPlayer.animation_finished
-		$EditableLevel/Camera2D.follow_node = %Playerd
+		$EditableLevel/Camera2D.follow_node = %Player
 	pass
+
+func start_animation_finished() -> void:
+	%CameraShaker.shake_strength = Vector2(0, 15.0)
+	%CameraShaker.shake_decay_rate = 1.0
+	await get_tree().create_timer(0.5).timeout
+	%CameraShaker.shake_strength = Vector2.ZERO
