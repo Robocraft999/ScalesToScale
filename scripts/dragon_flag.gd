a762f8ddeae78f46ee7c2def3d519d36ed7e49e6
VegOwOtenks <vegotenksGogeto@gmx.de>
2024-08-18 14:51:03 +0200

    Weird Godot file stuff

---
+++ scripts/dragon_flag.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+signal player_entered(body: Area2D)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	pass
+
+func _on_body_entered(body: Node2D):
+	if body is Player:
+		player_entered.emit(body)
+		pass
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ scripts/end_animator.gd
@@ -0,0 +1,20 @@
+extends Node
+class_name EndAnimator
+
+@onready var flag = $"../DragonFlag"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	flag.player_entered.connect(on_flag_player_touched)
+
+func on_flag_player_touched(player: Node2D) -> void:
+	var cam: BetterCamera2D = $"../Camera2D"
+	cam.follow_node = flag
+	cam.dead_zone = Vector2.ZERO
+	var tween = create_tween()
+	tween.tween_property(cam, "zoom", Vector2(3,3), 3)
+	tween.chain().tween_property(player, "position", Vector2(player.position.x, 0), 3)
+	tween.finished.connect(func(): SceneLoader.load_level_scene_by_name("MainMenu"))
+	await get_tree().create_timer(0.5).timeout
+	player.allow_movement = false
+	pass
