ac7044b7bc9660c0fa1e23ff141fcce5bde11763
VegOwOtenks <vegotenksGogeto@gmx.de>
2024-08-19 14:33:19 +0200

    Level 3 FINISH

--- scripts/single-use/vertical_scale_unlocker.gd
+++ scripts/single-use/vertical_scale_unlocker.gd
@@ -1,4 +1,8 @@
 extends Node2D
 
 func handle(_node: Player):
+	var label = $"../VerticalScaleLabel"
 	ProgressStore.vertical_scale_enabled
+	label.visible_ratio = 0.0
+	create_tween().bind_node(label).set_trans(Tween.TRANS_LINEAR) \
+		.tween_property(label, "visible_ratio", 1.0, 3.0)
