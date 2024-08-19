0c56a6dc965a2549167a16d4ca1f55084cd9a212
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2024-08-20 01:36:58 +0200

    added some shaders for level end

--- scripts/dragon_flag.gd
+++ scripts/dragon_flag.gd
@@ -10,5 +10,12 @@ func _ready() -> void:
 func _on_body_entered(body: Node2D):
 	if body is Player:
 		player_entered.emit(body)
+		$GodRays.visible = true
+		$CanvasLayer.visible = true
+		var darken_shader: ShaderMaterial = $CanvasLayer/DarkenShader.material
+		create_tween().tween_method(func(alpha): darken_shader.set_shader_parameter("alpha", alpha), 0.0, 0.4, 2)
+		var god_rays_shader: ShaderMaterial = $GodRays.material
+		create_tween().set_trans(Tween.TRANS_SINE).tween_method(func(cutoff): god_rays_shader.set_shader_parameter("cutoff", cutoff), 1.0, 0.0, 1)
 		pass
 	pass
+	
