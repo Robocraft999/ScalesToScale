f6ed9f6261d04b49bd73926475bbc252864355dd
= <vegotenksGogeto@gmx.de>
2024-08-19 21:37:42 +0200

    Better scale pickup

--- scripts/progress.gd
+++ scripts/progress.gd
@@ -4,6 +4,6 @@ extends Node
 var jump_enabled := not false
 var double_jump_enabled := false
 var dash_enabled := false
-var horizontal_scale_enabled := false
+var horizontal_scale_enabled := not false
 var vertical_scale_enabled := false
 var time_scale_enabled := true
--- scripts/single-use/double_jump_unlocker.gd
+++ scripts/single-use/double_jump_unlocker.gd
@@ -2,7 +2,13 @@ extends Node2D
 
 func handle(_node: Player):
 	ProgressStore.double_jump_enabled = true
+	self.visible = true
 	var label = $"../DoubleJumpLabel"
-	create_tween().bind_node(label).set_trans(Tween.TRANS_LINEAR) \
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_LINEAR) \
+		.tween_property($Label, "visible_ratio", 1.0, 1.0)
+	tween.chain().set_trans(Tween.TRANS_LINEAR) \
 		.tween_property(label, "visible_ratio", 1.0, 3.0)
 	$"../EditableLevel/Camera2D".follow_node = $"../DoubleJumpFocusPoint"
+	await get_tree().create_timer(3.5).timeout
+	$Label.visible = false
