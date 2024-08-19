0518c2877c7950daf429ea427414ab3c125d60b0
VegOwOtenks <vegotenksGogeto@gmx.de>
2024-08-19 11:46:22 +0200

    Cave in level 1

--- scripts/levels/level_1.gd
+++ scripts/levels/level_1.gd
@@ -5,3 +5,6 @@ func start_animation_finished() -> void:
 	%CameraShaker.shake_decay_rate = 1.0
 	await get_tree().create_timer(0.5).timeout
 	%CameraShaker.shake_strength = Vector2.ZERO
+
+func handle(_body: Player):
+	ProgressStore.horizontal_scale_enabled = true
