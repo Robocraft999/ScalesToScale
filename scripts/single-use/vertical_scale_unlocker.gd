990847bd29ea1082a1419d5723e2b08669d59e07
VegOwOtenks <vegotenksGogeto@gmx.de>
2024-08-19 15:44:51 +0200

    Removed No-Effect-Statement bug

--- scripts/single-use/vertical_scale_unlocker.gd
+++ scripts/single-use/vertical_scale_unlocker.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func handle(_node: Player):
 	var label = $"../VerticalScaleLabel"
-	ProgressStore.vertical_scale_enabled
+	ProgressStore.vertical_scale_enabled = true
 	label.visible_ratio = 0.0
 	create_tween().bind_node(label).set_trans(Tween.TRANS_LINEAR) \
 		.tween_property(label, "visible_ratio", 1.0, 3.0)
