e1994c4182c12d5b09d8e9a7bd55b714af5ec4e2
VegOwOtenks <vegotenksGogeto@gmx.de>
2024-08-19 13:22:16 +0200

    Double jump tutorial in level 3

--- scripts/levels/level_3.gd
+++ scripts/levels/level_3.gd
@@ -1,1 +1,15 @@
 extends Node2D
+
+func _ready() -> void:
+	$DoubleJumpExitArea.body_entered.connect(_double_jump_area_exited)
+	
+
+func _process(_delta: float) -> void:
+	$DoubleJumpFocusPoint.position.y = %Player.position.y
+	pass
+
+func _double_jump_area_exited(body: Node2D):
+	if body is Player:
+		$EditableLevel/Camera2D.follow_node = %Player
+		$DoubleJumpExitArea.body_entered.disconnect(_double_jump_area_exited)
+		pass
--- scripts/single-use/double_jump_unlocker.gd
+++ scripts/single-use/double_jump_unlocker.gd
@@ -1,4 +1,8 @@
 extends Node2D
 
-func handle(node: Player):
+func handle(_node: Player):
 	ProgressStore.double_jump_enabled = true
+	var label = $"../DoubleJumpLabel"
+	create_tween().bind_node(label).set_trans(Tween.TRANS_LINEAR) \
+		.tween_property(label, "visible_ratio", 1.0, 3.0)
+	$"../EditableLevel/Camera2D".follow_node = $"../DoubleJumpFocusPoint"
