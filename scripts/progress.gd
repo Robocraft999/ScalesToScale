8fa9c56e148339310693c944da77db233ba3ae44
VegOwOtenks <vegotenksGogeto@gmx.de>
2024-08-20 01:37:20 +0200

    Tweak shaker scripts

--- scripts/progress.gd
+++ scripts/progress.gd
@@ -2,8 +2,8 @@ extends Node
 
 # TODO: Change before release to false
 var jump_enabled := not false
-var double_jump_enabled := false
+var double_jump_enabled := not false
 var dash_enabled := false
 var horizontal_scale_enabled := not false
-var vertical_scale_enabled := false
+var vertical_scale_enabled := not false
 var time_scale_enabled := true
