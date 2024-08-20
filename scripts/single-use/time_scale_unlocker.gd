7d42defcbac5b0bd46ea279714c82c0e16507014
= <vegotenksGogeto@gmx.de>
2024-08-20 17:23:39 +0200

    Level 5 improvement

--- scripts/single-use/time_scale_unlocker.gd
+++ scripts/single-use/time_scale_unlocker.gd
@@ -2,3 +2,8 @@ extends Node2D
 
 func handle(_player: Player):
 	ProgressStore.time_scale_enabled = true
+	$"../NameLabel".visible_ratio = 0.0
+	var tween = create_tween().set_ease(Tween.EASE_OUT_IN).set_trans(Tween.TRANS_LINEAR)
+	
+	tween.tween_property($"../NameLabel", "visible_ratio", 1.0, 1.0)
+	tween.chain().tween_property($"../ExplanationLabel", "visible_ratio", 1.0, 3.0)
