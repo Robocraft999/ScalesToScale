928ba8c12a5fede2b01ef07e45ace54b63eb255d
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2024-08-20 12:20:38 +0200

    fixed wallglue (trust me)

--- scripts/wall_glue.gd
+++ scripts/wall_glue.gd
@@ -3,11 +3,20 @@ class_name WallGlue
 
 
 @export var direction: Vector2 = Vector2.ZERO
+var parent
+
+func _ready() -> void:
+	get_tree().create_timer(0.1).timeout.connect(func(): parent = get_parent())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	var parent = get_parent()
+	#var parent = get_parent()
+	if not parent:
+		return
+	
+	parent.collision_mask = 1
 	if not parent.test_move(parent.transform, direction):
 		parent.position += direction
 		pass
 	pass
+	parent.collision_mask = 3
