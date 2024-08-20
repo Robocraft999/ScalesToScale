97642035f727553ab23fd10c6a5b6985fb268048
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2024-08-20 14:18:54 +0200

    fixed death area
    disabled player movement on contact

--- scripts/death_area.gd
+++ scripts/death_area.gd
@@ -5,11 +5,11 @@ func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		print("Tot haha (You found an easter egg)")
 		var player: Player = body
+		player.allow_movement = false
 		
 		if Global.checkpoint_id >= 0:
 			await SceneLoader.reload_to_checkpoint()
 			player.global_position = Global.checkpoint_position
-			player.allow_movement = false
 			await SceneLoader.on_transition_ended()
 			player.allow_movement = true
 		else:
