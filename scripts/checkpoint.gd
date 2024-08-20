4449b9b6a0d0c2eb3906a90088c9c1f6c945c151
VegOwOtenks <vegotenksGogeto@gmx.de>
2024-08-20 14:15:00 +0200

    Signal for checkpoint

--- scripts/checkpoint.gd
+++ scripts/checkpoint.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal checkpoint_saved
+
 ## greater values are later checkpoints
 @export var id := 0
 var checked := false
@@ -13,4 +15,4 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 			checked = true
 			Global.checkpoint_position = global_position + Vector2(0, - body.get_child(0).shape.get_rect().size.y/2 - 5)
 			Global.checkpoint_id = id
-			
+			checkpoint_saved.emit()
