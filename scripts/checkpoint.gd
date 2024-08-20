317f375dd5e60101a2ca9158204e1d3fff417be4
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2024-08-20 13:25:33 +0200

    added particles to checkpoints

--- scripts/checkpoint.gd
+++ scripts/checkpoint.gd
@@ -2,11 +2,15 @@ extends Node2D
 
 ## greater values are later checkpoints
 @export var id := 0
+var checked := false
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		if Global.checkpoint_id <= id:
+			if not checked:
+				$GPUParticles2D.emitting = true
+			checked = true
 			Global.checkpoint_position = global_position + Vector2(0, - body.get_child(0).shape.get_rect().size.y/2 - 5)
 			Global.checkpoint_id = id
 			
