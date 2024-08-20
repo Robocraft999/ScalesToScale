ef3d5a1a7df830382c2e455c6eba588815cf83c6
VegOwOtenks <vegotenksGogeto@gmx.de>
2024-08-20 15:49:50 +0200

    Level 6 FINISH #1

--- scripts/levels/level_6.gd
+++ scripts/levels/level_6.gd
@@ -10,3 +10,18 @@ func _ready() -> void:
 	ProgressStore.vertical_scale_enabled = true
 	ProgressStore.time_scale_enabled = true
 	%Player.get_node("AnimatedSprite2D").play("fly")
+	$Checkpoints/Checkpoint2.connect("checkpoint_saved", _fire_platform1)
+	$Checkpoints/Checkpoint3.checkpoint_saved.connect(_fire_double_spikes)
+	$Checkpoints/Checkpoint4.checkpoint_saved.connect(_start_final_animation)
+
+func _start_final_animation():
+	$MovingPlatform/EndPlatformMover.go()
+
+func _fire_double_spikes():
+	$MovingSpikes/Mover3.go()
+	$MovingSpikes/Mover4.go()
+	$MovingSpikes/Mover5.go()
+	$Checkpoints/Checkpoint3.checkpoint_saved.disconnect(_fire_double_spikes)
+
+func _fire_platform1():
+	$MovingPlatform/DangerMover.go()
--- scripts/mover.gd
+++ scripts/mover.gd
@@ -6,17 +6,24 @@ extends Path2D
 @export_category("Tweening")
 @export var trans := Tween.TRANS_SINE
 @export var ease_mode := Tween.EASE_IN_OUT
+@export var autostart := true
+@export var loop := true
 
 func _ready():
 	_follow.progress_ratio = 0
 	var remote: RemoteTransform2D = $PathFollow2D/RemoteTransform2D
 	remote.remote_path = target_node.get_path()
-	go()
+	if autostart:
+		go()
 
 func go():
 	var tween = create_tween().set_trans(trans).set_ease(ease_mode)
-	tween.tween_property(_follow, 'progress_ratio', 1, SPEED / Global.time_scale).finished.connect(go_back)
+	var propertytween = tween.tween_property(_follow, 'progress_ratio', 1, SPEED / Global.time_scale)
+	if loop:
+		propertytween.finished.connect(go_back)
 
 func go_back():
 	var tween = create_tween().set_trans(trans).set_ease(ease_mode)
-	tween.tween_property(_follow, "progress_ratio", 0, SPEED / Global.time_scale).finished.connect(go)
+	var propertytween = tween.tween_property(_follow, "progress_ratio", 0, SPEED / Global.time_scale)
+	if loop:
+		propertytween.finished.connect(go)
