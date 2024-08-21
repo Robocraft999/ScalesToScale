1c0d9a9169081993d15e5f938d4d9aca9282e1ce
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2024-08-21 13:38:49 +0200

    added partially controller support

--- scripts/credits_menu.gd
+++ scripts/credits_menu.gd
@@ -1,4 +1,7 @@
 extends Control
 
+func _ready() -> void:
+	%MenuButton.grab_focus()
+
 func _on_menu_button_pressed() -> void:
 	SceneLoader.load_level_scene_by_name(Global.SceneName.MAIN_MENU, false)
--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,5 +1,8 @@
 extends CenterContainer
 
+func _ready() -> void:
+	$VBoxContainer/MainMenu.grab_focus()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
